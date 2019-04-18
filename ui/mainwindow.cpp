e615913ab7cc096c5609f73f388559e592edd743
Schulte, Eberhard <schulte@sigma3d.de>
2019-04-18 10:56:42 +0200

    OI-157: show MesseageBox if project / job is not saved

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -1219,21 +1219,27 @@ void MainWindow::on_actionClose_triggered(){
 
 void MainWindow::closeEvent(QCloseEvent *event)
 {
-    QMessageBox msgBox;
-    msgBox.setWindowTitle("close application");
-    msgBox.setText("Do you want to save changes?");
-    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
-    msgBox.setDefaultButton(QMessageBox::Yes);
+    // direct call (no "emit" use), therefore is no need to handle the event in the controller
+    if(!this->control.hasProjectDigestChanged()) {
 
-    int ret = msgBox.exec();
+        QMessageBox msgBox;
+        msgBox.setWindowTitle("close application");
+        msgBox.setText("Do you want to save changes?");
+        msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
+        msgBox.setDefaultButton(QMessageBox::Yes);
 
-    if(ret == QMessageBox::Yes){
-        emit this->saveProject();
+        int ret = msgBox.exec();
+
+        if(ret == QMessageBox::Yes){
+            emit this->saveProject();
+            event->accept();
+        }else if(ret == QMessageBox::No){
+            event->accept();
+        }else if(ret == QMessageBox::Cancel){
+            event->ignore();
+        }
+    } else {
         event->accept();
-    }else if(ret == QMessageBox::No){
-        event->accept();
-    }else if(ret == QMessageBox::Cancel){
-        event->ignore();
     }
 }
 
