15049cd2ecf3126b090e7eb845b4e59ea2cd12af
Schulte, Eberhard <schulte@sigma3d.de>
2019-07-10 09:25:11 +0200

    OI-533: set label and shortcut

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -2681,11 +2681,12 @@ void MainWindow::activeSensorTypeChanged(const SensorTypes &type, const QList<Se
         QString actionCommand = rx.cap(1);
 
         QPointer<QAction> customAction = new QAction(0);
-        customAction->setText(action);
+        customAction->setShortcut(actionShortCut);
+        customAction->setText(actionLabel);
         this->selfDefinedActions.append(customAction);
         this->ui->toolBar_controlPad->addAction(customAction.data());
         QObject::connect(customAction.data(), SIGNAL(triggered()), this->customActionMapper.data(), SLOT(map()), Qt::AutoConnection);
-        this->customActionMapper->setMapping(customAction.data(), action);
+        this->customActionMapper->setMapping(customAction.data(), actionCommand);
     }
     QObject::connect(this->customActionMapper.data(), SIGNAL(mapped(const QString&)), &this->control, SLOT(startCustomAction(const QString&)), Qt::AutoConnection);
 
