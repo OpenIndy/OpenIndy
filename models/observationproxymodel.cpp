86f8c7f721b15647c6baa2491b2f7d62f97612b5
Schulte, Eberhard <schulte@sigma3d.de>
2019-11-04 10:53:29 +0100

    OI-629: fix: make column sortable

--- models/observationproxymodel.cpp
+++ models/observationproxymodel.cpp
@@ -84,16 +84,19 @@ bool ObservationProxyModel::lessThan(const QModelIndex &source_left, const QMode
         QPointer<Function> function = source_model->getCurrentJob()->getActiveFeature()->getFeature()->getFunctions().at(0);
         QList<InputElement> inputElem = function->getInputElements().value(0);
 
-        if(!source_right.isValid() || !source_left.isValid()
-                || source_right.row() <= inputElem.size() || source_left.row() <= inputElem.size()
-                || inputElem.at(source_right.row()).observation.isNull() || inputElem.at(source_left.row()).observation.isNull()){
-            return false;
+        if( source_right.isValid()
+            && source_left.isValid()
+            && source_right.row() < inputElem.size()
+            && source_left.row() < inputElem.size()
+            && !inputElem.at(source_right.row()).observation.isNull()
+            && !inputElem.at(source_left.row()).observation.isNull()){
+
+            int fwRight = inputElem.at(source_right.row()).observation->getId();
+            int fwLeft =  inputElem.at(source_left.row()).observation->getId();
+
+            return fwLeft > fwRight;
         }
 
-        int fwRight = inputElem.at(source_right.row()).observation->getId();
-        int fwLeft =  inputElem.at(source_left.row()).observation->getId();
-
-        return fwLeft > fwRight;
     }
     return false;
 
