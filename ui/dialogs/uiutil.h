21cd87108e60c431fe964cc5406f72ee66af8970
Schulte, Eberhard <schulte@sigma3d.de>
2020-01-29 08:35:49 +0100

    OI-642: refactoring

---
+++ ui/dialogs/uiutil.h
@@ -0,0 +1,18 @@
+#ifndef UIUTIL_H
+#define UIUTIL_H
+
+#include <QString>
+#include <QStringList>
+#include <QMap>
+#include <QFont>
+#include <QFontMetrics>
+
+#include "types.h"
+
+namespace oi{
+
+const int OI_CORE_EXPORT getDropDownMenuSize(QStringList list, const int menuSize);
+
+}
+
+#endif // UIUTIL_H
