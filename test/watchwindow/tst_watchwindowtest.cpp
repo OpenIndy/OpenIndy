f0f2eb900072c1ec1b9e800905b2c93f52960730
Schulte, Eberhard <schulte@sigma3d.de>
2020-01-30 07:53:05 +0100

    OI-642: verfiy result

--- test/watchwindow/tst_watchwindowtest.cpp
+++ test/watchwindow/tst_watchwindowtest.cpp
@@ -44,8 +44,8 @@ void WatchwindowTest::testPoint1()
 
     OiVec pos = OiVec(3);
     pos.setAt(0, 1.003);
-    pos.setAt(1, 1.004);
-    pos.setAt(2, 1.005);
+    pos.setAt(1, 1.005);
+    pos.setAt(2, 1.007);
 
     QPointer<Point> point = new Point(false, Position(pos));
     point->setIsSolved(true);
@@ -58,14 +58,14 @@ void WatchwindowTest::testPoint1()
     OiVec resultPosition = result.position.getVector();
     double resultRadius = result.radius.getRadius();
     QVERIFY2(resultPosition.getAt(0) == 1.003, "");
-    QVERIFY2(resultPosition.getAt(1) == 1.004, "");
-    QVERIFY2(resultPosition.getAt(2) == 1.005, "");
+    QVERIFY2(resultPosition.getAt(1) == 1.005, "");
+    QVERIFY2(resultPosition.getAt(2) == 1.007, "");
     QVERIFY2(resultRadius == 0, "radius");
 
-    COMPARE_DOUBLE(result.d3D, 0.0035, 0.0001);
+    COMPARE_DOUBLE(result.d3D, 0.0054, 0.0001);
     COMPARE_DOUBLE(result.delta.getAt(0), 0.002, 0.0001);
-    COMPARE_DOUBLE(result.delta.getAt(1), 0.002, 0.0001);
-    COMPARE_DOUBLE(result.delta.getAt(2), 0.002, 0.0001);
+    COMPARE_DOUBLE(result.delta.getAt(1), 0.003, 0.0001);
+    COMPARE_DOUBLE(result.delta.getAt(2), 0.004, 0.0001);
 
 
 }
