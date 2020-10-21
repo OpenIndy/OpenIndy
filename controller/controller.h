b948ec8d56e97af7386cbf9d6673b69bf9010b85
Schulte, Eberhard <schulte@sigma3d.de>
2020-10-21 15:52:18 +0200

    OI-666: adding signal setCurrentJob

--- controller/controller.h
+++ controller/controller.h
@@ -215,6 +215,8 @@ signals:
 
     //whole job instance changed
     void currentJobChanged();
+    // set current job to other modules / tools
+    void setCurrentJob(const QPointer<OiJob> &job);
 
     //#################################
     //import export task status changes
