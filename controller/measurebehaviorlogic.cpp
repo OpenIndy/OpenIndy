e8139117691b71c43dc395b291333fd666464521
Schulte, Eberhard <schulte@sigma3d.de>
2022-02-10 13:08:25 +0100

    OI-884: aim, search and measure logic

--- controller/measurebehaviorlogic.cpp
+++ controller/measurebehaviorlogic.cpp
@@ -1,8 +1,9 @@
 #include "measurebehaviorlogic.h"
 
-MeasureBehaviorLogic::MeasureBehaviorLogic(QObject *parent) : QObject(parent)
+MeasureBehaviorLogic::MeasureBehaviorLogic(QObject *parent) : QObject(parent), activeFeatureId(-1)
 {
-
+    QObject::connect(&this->measureBehaviorDialog, &QDialog::accepted, this, &MeasureBehaviorLogic::handleDecision);
+    QObject::connect(&this->measureBehaviorDialog, &QDialog::rejected, this, &MeasureBehaviorLogic::abortMeasurement);
 }
 
 void MeasureBehaviorLogic::init(ControllerSensorActions *control, QList<int> measureFeatures, FeatureTableModel *sourceModel) {
@@ -11,70 +12,103 @@ void MeasureBehaviorLogic::init(ControllerSensorActions *control, QList<int> mea
     this->control = control;
     this->measureFeatures = measureFeatures;
     this->sourceModel = sourceModel;
+    this->activeFeatureId = -1;
 }
 
 /**
- * aim and measure current feature in the list of selected features
+ * aim, search (optional) and measure active feature in the list of selected features
  *
- * @brief MeasureBehaviorLogic::measureNext
+ * @brief MeasureBehaviorLogic::measure
  */
 void MeasureBehaviorLogic::measure() {
 
-    if(this->measureFeatures.size() > 0) {
+    // aim / move
+    this->control->startAim();
 
-        // aim / move
-        this->control->startAim();
-
-        // search
-        if(this->measureBehaviorDialog.searchSMR()) {
-            this->control->startSearch();
-        }
-
-        // measure
-        this->control->startMeasurement();
-
+    // search
+    if(this->measureBehaviorDialog.searchSMR()) {
+        this->control->startSearch();
     }
+
+    // measure
+    this->control->startMeasurement();
 
 }
 
 /**
  * set next feature as active feature
+ *
  * @brief MeasureBehaviorLogic::next
  * @return true if next feature is available
  */
 bool MeasureBehaviorLogic::next() {
     if(this->measureFeatures.size() > 0) {
         setActiveFeature(this->sourceModel, this->measureFeatures[0]);
+        this->activeFeatureId = this->measureFeatures[0];
         this->measureFeatures.removeAt(0);
         return true;
+
+    } else {
+        this->activeFeatureId = -1;
+        return false;
     }
-    return false;
+
 }
 
+/**
+ * receives all "sensorActionFinished" signals and handles only "measure" signals
+ *
+ * @brief MeasureBehaviorLogic::sensorActionFinished
+ * @param success
+ * @param msg
+ */
 void MeasureBehaviorLogic::sensorActionFinished(const bool &success, const QString &msg) {
-    qDebug() << "MeasureBehaviorLogic::sensorActionFinished" << success << msg;
 
-    if(success && SensorWorkerMessage::MEASUREMENT_FINISHED != msg) {
+    if(activeFeatureId == -1) { // silent ignore
+        return;
+    }
+
+    if( success
+       && ( SensorWorkerMessage::MEASUREMENT_FINISHED == msg            // sync sensor
+            || SensorWorkerMessage::MEASUREMENT_DATA_RECEIVED == msg)   // async sensor
+       ) {
         if(this->next()) { // measure next feature if avialable
             this->measure();
         }
 
-    } else if(true){ // Fehlerfälle
+    } else if( !success
+              && ( SensorWorkerMessage::FAILED_TO_MEASURE == msg                            // sync sensor
+                   || SensorWorkerMessage::MEASUREMENT_DIT_NOT_DELIVER_ANY_RESULTS == msg)  // async sensor
+              ){
+        showCentered(this->measureBehaviorDialog); // QDialog::accepted calls handleDecision() by signal / slot
+    }
 
-        if(!this->measureBehaviorDialog.useDecision()) {
-            showCentered(this->measureBehaviorDialog);
-        }
+    // silent ignore all other cases
+}
 
-        if(this->measureBehaviorDialog.tryAgain()) {
+void MeasureBehaviorLogic::handleDecision() {
+
+    if(this->measureBehaviorDialog.tryAgain()
+            || this->measureBehaviorDialog.searchSMR()) {
+
+        this->measure();
+
+    } else if(this->measureBehaviorDialog.skip()) {
+        if(this->next()) {
             this->measure();
-
-        } else if(this->measureBehaviorDialog.skip()
-                  && this->next()) {
-            this->measure();
-
         }
 
     }
+
+}
+
+/**
+ * abort measurement
+ *
+ * @brief MeasureBehaviorLogic::abortMeasurement
+ */
+void MeasureBehaviorLogic::abortMeasurement() {
+    this->measureFeatures.clear();
 }
 
 void MeasureBehaviorLogic::showCentered(QDialog &dialog) {
