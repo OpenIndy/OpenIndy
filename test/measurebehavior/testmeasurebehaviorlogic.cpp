7b053bad30d8235919405f376098266db82e5f52
Schulte, Eberhard <schulte@sigma3d.de>
2022-02-11 11:34:15 +0100

    OI-884: test

--- test/measurebehavior/testmeasurebehaviorlogic.cpp
+++ test/measurebehavior/testmeasurebehaviorlogic.cpp
@@ -7,8 +7,10 @@ TestMeasureBehaviorLogic::TestMeasureBehaviorLogic()
 
 void TestMeasureBehaviorLogic::showCentered(QDialog &dialog) {
     qDebug() << "showCentered";
+    this->handleDecision();
 }
 
 void TestMeasureBehaviorLogic::setActiveFeature(FeatureTableModel *sourceModel, int featureId) {
     qDebug() << "setActiveFeature: featureId: " << featureId;
+    this->featureIds.append(featureId);
 }
