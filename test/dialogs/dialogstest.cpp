8fe776a528ceef01e1f2b790ee9a1386c4b66bb3
Schulte, Eberhard <schulte@sigma3d.de>
2022-07-27 10:34:09 +0200

    OI-492: test: select combobox entry

--- test/dialogs/dialogstest.cpp
+++ test/dialogs/dialogstest.cpp
@@ -2,8 +2,10 @@
 #include <QtTest>
 #include <QPointer>
 #include <QList>
+#include <QStringList>
+#include <QStringListModel>
 #include "createfeaturedialog.h"
-
+#include "availablefunctionslistproxymodel.h"
 class DialogsTest : public QObject
 {
     Q_OBJECT
@@ -23,12 +25,48 @@ DialogsTest::DialogsTest() {
 
 }
 
+// https://gist.github.com/peteristhegreat/cbd8eaa0e565d0b82dbfb5c7fdc61c8d
+// https://vicrucann.github.io/tutorials/qttest-signals-qtreewidget/
 void DialogsTest::initial()
 {
 
+    QStringList entityTypes;
+    entityTypes << "point" << "circle" << "plane" << "level" << "cylinder";
+
+    QPointer<MeasurementConfigManager> measurementConfigManager = new MeasurementConfigManager();
+
+    QList<sdb::Plugin> plugins;
+
+    ModelManager::testInit(entityTypes, measurementConfigManager, plugins);
+
     CreateFeatureDialog dialog;
 
-    qDebug() << "initial";
+    QPointer<QComboBox> entityTypeCB = dialog.findChild<QComboBox *>("comboBox_entityType");
+    qDebug() << entityTypeCB;
+    qDebug() << entityTypeCB->currentIndex();
+    QVERIFY(0 == entityTypeCB->currentIndex());
+    QTest::mouseClick(entityTypeCB, Qt::LeftButton);
+    QTest::qWait(500);
+
+    QPointer<QListView> entityTypeLV = entityTypeCB->findChild<QListView *>();
+    qDebug() << entityTypeLV;
+
+    int r = 1;
+    QModelIndex idx = entityTypeLV->model()->index(r,0);
+    entityTypeLV->scrollTo(idx);
+
+    QPoint itemPt = entityTypeLV->visualRect(idx).center();
+    QString str = entityTypeLV->model()->index(r,0).data( Qt::DisplayRole ).toString();
+    qDebug() << "clicking on" << str ;
+
+    QTest::mouseClick(entityTypeLV->viewport(), Qt::LeftButton, 0, itemPt);
+    QTest::qWait(500);
+    // Reopen the combobox
+    QTest::mouseClick(entityTypeCB, Qt::LeftButton);
+    QTest::qWait(500);
+
+    qDebug() << entityTypeCB->currentIndex();
+    QVERIFY(1 == entityTypeCB->currentIndex());
 }
 
 QTEST_MAIN(DialogsTest)
