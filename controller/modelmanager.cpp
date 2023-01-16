6f0d75090ee243a6d3092b2dc1b339eda7dfef18
Schulte, Eberhard <schulte@sigma3d.de>
2023-01-16 07:09:01 +0100

    OI-847: label for type

--- controller/modelmanager.cpp
+++ controller/modelmanager.cpp
@@ -1372,6 +1372,7 @@ void ModelManager::initReadingTypesModels(){
 
     QStringList readingTypes;
     readingTypes.append(getReadingTypeName(eCartesianReading));
+    readingTypes.append(getReadingTypeName(eCartesianReading6D));
     readingTypes.append(getReadingTypeName(eDistanceReading));
     readingTypes.append(getReadingTypeName(ePolarReading));
     readingTypes.append(getReadingTypeName(eDirectionReading));
