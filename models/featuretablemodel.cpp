43e2babdb966ba9d081091156f318b998a44208e
Schulte, Eberhard <schulte@sigma3d.de>
2023-06-22 11:55:04 +0200

    OI-1001: copy observations to feature

--- models/featuretablemodel.cpp
+++ models/featuretablemodel.cpp
@@ -499,17 +499,23 @@ bool FeatureTableModel::setData(const QModelIndex & index, const QVariant & valu
                                     copyFunction->getScalarInputParams());
                         } // <- copy scalar input params
 
+                        QPointer<Geometry> geometry = feature->getGeometry();
                         // 3.3.2 copy used elements
                         if(editMode & EditMode::eFunctionCopyUsedElements) {
                             QMap<int, QList<InputElement> > inputElements = copyFunction->getInputElements();
                             QMap<int, QList<InputElement> >::const_iterator iterator = inputElements.constBegin();
+                            if(!inputElements.isEmpty()) { // remove existing observation references
+                                for(QPointer<Observation> obs : geometry->getObservations()) {
+                                    geometry->removeObservation(obs);
+                                }
+                            }
                             while (iterator != inputElements.constEnd()) {
                                 QList<InputElement> elements = iterator.value();
                                 for (int i = 0; i < elements.size(); ++i) {
                                     InputElement element = elements.at(i);
                                     function->addInputElement(element, iterator.key());
-                                    if(!element.observation.isNull()) {
-                                        feature->getGeometry()->addObservation(element.observation);
+                                    if(!element.observation.isNull()) {  // insert observation reference from function to feature
+                                        geometry->addObservation(element.observation);
                                     }
                                 }
                                 ++iterator;
