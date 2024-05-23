de8c09bd02478ac18657fb1c963ac5ed29486b9a
Schulte, Eberhard <schulte@sigma3d.de>
2024-05-23 12:10:28 +0200

    OI-603: refactoring

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -1,5 +1,6 @@
 #include "controller.h"
 #include "defaultsconfiginit.h"
+#include "featureutil.h"
 /*!
  * \brief Controller::Controller
  * \param parent
@@ -613,50 +614,10 @@ void Controller::addBundleSystem(){ // TODO OI-932 remove method / OI-1009 maybe
     //add feature
     this->job->addFeatures(bundleAttr);
 
-    // adapted from FeatureUpdater::createBundleTransformations
-    // create intial bundle transformation (Bundle01_1) to STATION01
-    {
-
-        FeatureAttributes trafoAttr;
-        trafoAttr.typeOfFeature = eTrafoParamFeature;
-        trafoAttr.group = "";
-        trafoAttr.count = 1;
-        trafoAttr.startSystem = bundleName;
-        trafoAttr.name = QString("%1_1").arg(bundleName);
-        trafoAttr.destinationSystem = "STATION01";
-
-        QList<QPointer<FeatureWrapper> > features = this->job->addFeatures(trafoAttr);
-        if(features.isEmpty()) {
-            this->log("initial bundle not created", eWarningMessage, eConsoleMessage);
-            return;
-        }
-        QPointer<FeatureWrapper> trafoParam = features.first();
-
-        //set up transformation parameters
-        if(trafoParam.isNull() || trafoParam->getTrafoParam().isNull()) {
-            this->log("initial bundle is null", eWarningMessage, eConsoleMessage);
-            return;
-        }
-
-        QPointer<TrafoParam> trafo = trafoParam->getTrafoParam();
-
-        //set up parameters
-        QMap<TrafoParamParameters, double> parameters;
-        parameters.insert(eUnknownTX, 0.0);
-        parameters.insert(eUnknownTY, 0.0);
-        parameters.insert(eUnknownTZ, 0.0);
-        parameters.insert(eUnknownRX, 0.0);
-        parameters.insert(eUnknownRY, 0.0);
-        parameters.insert(eUnknownRZ, 0.0);
-        parameters.insert(eUnknownSX, 1.0);
-        parameters.insert(eUnknownSY, 1.0);
-        parameters.insert(eUnknownSZ, 1.0);
-        trafo->setUnknownParameters(parameters);
-        trafo->setIsUsed(true);
-        trafo->setIsDatumTrafo(true);
-
+    FeatureUtil util;
+    if(!util.createInitialBundleTransformation(job)) {
+        this->log("initial bundle not created", eWarningMessage, eConsoleMessage);
     }
-
 }
 
 /*!
