606dd9c8c541034156ee9bbf0befb0de894e7602
Schulte, Eberhard <schulte@sigma3d.de>
2024-06-11 11:22:15 +0200

    OI-669: fix: sendMessage

--- models/bundleparametertableproxymodel.h
+++ models/bundleparametertableproxymodel.h
@@ -33,7 +33,7 @@ signals:
     //send messages to OpenIndy
     //#########################
 
-    void sendMessage(const QString &msg, const MessageTypes &msgType, const MessageDestinations &msgDest = eConsoleMessage);
+    void sendMessage(const QString &msg, const MessageTypes &msgType, const MessageDestinations &msgDest = eConsoleMessage) const;
     
 protected:
 
